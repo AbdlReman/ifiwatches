9ba25a99335f8b371a8bc2245bd815a4e3b0e847
AbdlReman <abdulrehmanabbasi5067794@gmail.com>
2026-06-07 05:51:47 -0700

    update

--- app/(public)/shop/ShopClient.tsx
+++ app/(public)/shop/ShopClient.tsx
@@ -345,14 +345,14 @@ export default function ShopClient({
               </Link>
             </div>
             {/* Cart link visible on mobile in top bar area */}
-            <div className="flex md:hidden">
+            {/* <div className="flex md:hidden">
               <Link
                 href="/cart"
                 className="inline-flex items-center justify-center border border-black bg-black px-4 py-2 text-center text-xs font-bold uppercase tracking-widest text-white hover:bg-neutral-800"
               >
                 Cart
               </Link>
-            </div>
+            </div> */}
           </div>
 
           <div>
